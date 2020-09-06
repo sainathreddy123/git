ac8a94e40de692247ec3847136dc0458d8cd4336
sainath reddy <root@devoops.localdomain>
2020-09-05 22:10:52 -0400

    test pom.xml

---
+++ maven/pom/tomcat/src/main/java/deployment/App.java
@@ -0,0 +1,13 @@
+package deployment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
